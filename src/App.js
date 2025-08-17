b17908a5d0bfe3e9422219b0418bfc2404bf804c
Shuvendu Dhenki <shuvendu.dhenki@gmail.com>
2025-08-17 13:27:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Mainsearch from "./components/Mainsearch";
+import Productlist from "./components/Productlist";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Mainsearch />
+      <Productlist />
+    </>
+  );
+}
+
+export default App;
